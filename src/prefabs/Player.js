90d7a44fb52f42768db3b335653c289a421d5238
BarkingTiger <neodragon057@gmail.com>
2022-05-15 17:15:35 -0700

    adding player.js

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -9,29 +9,41 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     update() {
 
         if (Phaser.Input.Keyboard.JustDown(keyUp)) {
-
+            this.setVelocityY(10);
         }
 
         if (Phaser.Input.Keyboard.JustDown(keyDown)) {
-
+            this.setVelocityY(-10);
         }
 
         if (Phaser.Input.Keyboard.JustDown(keyLeft)) {
-
+            this.setVelocityX(-10);
         }
 
         if (Phaser.Input.Keyboard.JustDown(keyRight)) {
-
+            this.setVelocityX(-10);
         }
 
         if (Phaser.Input.Keyboard.JustDown(keySpace)) {
 
         }
 
+        /*
         if (game.input.activePointer.leftButton.isDown) {
-            
+            shoot();
         }
-        
+        */
+
+        /*
+        shoot() {
+            if (game.time.now > nextFire && bullets.countDead() > 0) {
+                nextFire = game.time.now + fireRate;
+                var bullet = bullets.getFirstDead();
+                bullet.reset(sprite.x - 8, sprite.y - 8);
+                game.physics.arcade.moveToPointer(bullet, 300);
+            }
+        }
+        */
     }
 
     
